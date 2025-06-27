c30172fa79428cbec2ed003eff47e9d163f14f6e
Saad <saadbinmontasir11@gmail.com>
2025-06-27 11:56:47 +0600

    Update sumetion_arr.cpp

--- sumetion_arr.cpp
+++ sumetion_arr.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
-#include<cmath>
+#include<cstdlib>
 #include<vector>
 using namespace std;
 int main()
-{int n,sum=0,i;
+{long long n,sum=0,i;
 cin>>n;
 //int a[n];
-vector<int>a(n);
+vector<long long>a(n);
 for( i=1;i<=n;i++)
 {cin>>a[i];}
 
 for(int i=1;i<=n;i++)
 { sum+=a[i];}
-cout<< abs(sum);
+cout<< llabs(sum);
 }
